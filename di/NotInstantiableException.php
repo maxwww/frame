5b3cf8f0b45cd357f2becf1973c0228c7182f782
Maksym <ms.mikro.1@gmail.com>
2017-05-29 00:50:31 +0300

    add NotInstantiableException class

---
+++ di/NotInstantiableException.php
@@ -0,0 +1,29 @@
+<?php
+
+
+namespace msf\di;
+
+
+use msf\base\Exception;
+
+class NotInstantiableException extends Exception
+{
+    /**
+     * @inheritdoc
+     */
+    public function __construct($class, $message = null, $code = 0, \Exception $previous = null)
+    {
+        if ($message === null) {
+            $message = "Can not instantiate $class.";
+        }
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return string the user-friendly name of this exception
+     */
+    public function getName()
+    {
+        return 'Not instantiable';
+    }
+}
