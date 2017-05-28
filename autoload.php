6deac0cf2d3389edca421587aef4fe951a41969f
Maksym <ms.mikro.1@gmail.com>
2017-05-28 16:44:30 +0300

    reset

--- autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-function __autoload($class)
-{
-    $class = str_replace("\\", "/", $class);
-    if (preg_match('/^msf\//', $class)) {
-        require_once(__DIR__ . "/$class.php");
-    }
-}
