4d352d45201f751238f19276c62bb9b246f99fc0
Maksym <ms.mikro.1@gmail.com>
2017-05-28 17:41:58 +0300

    Add composer.json

---
+++ BaseMsf.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace msf;
+
+/**
+ * Class BaseMsf
+ * @package msf
+ *
+ * Class contains static methods and properties
+ */
+class BaseMsf
+{
+
+}
---
+++ Msf.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Class Msf
+ *
+ * Class contains static methods and properties
+ */
+class Msf extends \msf\BaseMsf
+{
+
+}
